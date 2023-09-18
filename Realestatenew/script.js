ccb3fe6aac209434b9c9d7b4cce9bbc7b31eb770
anisharawr <134849766+anisharawr@users.noreply.github.com>
2023-09-19 00:19:04 +0530

    Add files via upload

---
+++ Realestatenew/script.js
@@ -0,0 +1,98 @@
+
+
+
+
+/*
+  add event on element
+ */
+
+const addEventOnElement = function (element, type, listener) {
+  if (element.length > 1) {
+    for (let i = 0; i < element.length; i++) {
+      element[i].addEventListener(type, listener);
+    }
+  } else {
+    element.addEventListener(type, listener);
+  }
+}
+
+
+
+/*
+  navbar 
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+}
+
+addEventOnElement(navToggler, "click", toggleNav);
+
+
+const closeNav = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElement(navLinks, "click", closeNav);
+
+
+
+/*
+  add active class on header & back to top button
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 0) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+/*
+  hero tab button
+ */
+
+const tabBtns = document.querySelectorAll("[data-tab-btn]");
+
+let lastClickedTabBtn = tabBtns[0];
+
+const changeTab = function () {
+  lastClickedTabBtn.classList.remove("active");
+  this.classList.add("active");
+  lastClickedTabBtn = this;
+}
+
+addEventOnElement(tabBtns, "click", changeTab);
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2500); // Change image every 2 seconds
+}
